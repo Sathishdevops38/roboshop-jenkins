845025652fea1ed5b8394e985374b042c8bc5ecb
sathishdevops38 <sathishdevops38@gmail.com>
2025-12-24 15:49:35 +0530

    ansible

--- Terraform-jenkins-ec2/master.sh
+++ Terraform-jenkins-ec2/master.sh
@@ -11,6 +11,12 @@ xfs_growfs /
 xfs_growfs /var
 xfs_growfs /home
 
+#install ansible
+sudo dnf install ansible -y
+sudo systemctl start ansible
+sudo systemctl enable ansible
+
+#install terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 sudo yum -y install terraform
